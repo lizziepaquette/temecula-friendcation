cfeac7be089f52c2573dec6c688ac98a88069658
Jack Ricci <jricci@workramp.com>
2021-06-19 10:44:48 -0700

    update meta + completed background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import styled from 'styled-components';
 import Countdown from 'react-countdown';
-import temeculaMainPhoto from './photos/temecula-main-view.svg'
+import temeculaMainPhoto from './photos/temecula-main-view.svg';
+import funnyBwBackground from './photos/temecula-main-view-bw.svg';
 
 function App() {
   const temeculaTripStart = new Date('2021-08-13T16:00:00');
@@ -27,10 +28,13 @@ const CountdownClockWrapper = styled.div`
   background-image: url("${temeculaMainPhoto}");
 `;
 
+const CompletedWrapper = styled.div`
+  background-image: url("${funnyBwBackground}");
+`;
+
 const CountdownClock = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
-    return <h2>I really hope it was fun.</h2>;
-    // TODO: set background to ./photos/temecula-main-view-bw.svg here
+    return <CompletedWrapper><h2>I really hope it was fun.</h2></CompletedWrapper>;
   }
 
   return (
