a00396483f99fd641bf0ec3e6d8ac0b7b02b3a17
Jack Ricci <jricci@workramp.com>
2021-06-19 13:29:18 -0700

    background cover

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import funnyBwBackground from './photos/temecula-main-view-bw.svg';
 const CountdownHeader = styled.h1`
   font-size: 12vw;
   margin: 0px;
+
+  @media screen and (max-width: 768px) {
+    line-height: 200%
+  }
 `;
 
 const CountdownClockWrapper = styled.div`
@@ -14,8 +18,9 @@ const CountdownClockWrapper = styled.div`
   width: 100vw;
   color: white;
   background-image: url("${ props => props.bgImg }");
-  background-size: contain;
+  background-size: cover;
   box-sizing: border-box;
+  background-position: 30% 50%;
   background-repeat: no-repeat;
 `;
 
