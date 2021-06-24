e0ed2568f167665c18089af5b039439e9706d8e7
Lizzie Paquette <lizzie@brex.com>
2021-06-23 19:23:19 -0700

    split out calendar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Playlist from './components/Playlist'
 import MyCountdown from './components/MyCountdown'
 import AirbnbPreview from './components/AirbnbPreview'
 import GoogleMaps from './components/GoogleMaps'
+import GCalendar from './components/GCalendar'
 import Info from './components/Info'
 import { HashRouter, Route } from 'react-router-dom';
 
@@ -28,6 +29,11 @@ function App() {
         exact
         path='/google-maps'
         component={GoogleMaps}
+      />
+      <Route
+        exact
+        path='/calendar'
+        component={GCalendar}
       />
       <Route
         exact
--- src/components/Info.js
+++ src/components/Info.js
@@ -8,29 +8,10 @@ const Info = () => (
         </h1>
 
         <div>
-        Dates:
+            Dates:
             Friday ‌A‌u‌g‌u‌s‌t‌ ‌1‌3‌,‌ ‌2‌0‌2‌1‌ ‌C‌h‌e‌c‌k‌-‌i‌n‌ ‌i‌s‌ ‌4‌:‌0‌0‌ ‌P‌M‌ ‌-‌ ‌8‌:‌0‌0‌ ‌P‌M‌
             Thursday ‌A‌u‌g‌u‌s‌t‌ ‌1‌9‌,‌ ‌2‌0‌2‌1‌ ‌Ch‌e‌c‌k‌o‌u‌t‌ ‌b‌y‌ ‌1‌1‌:‌0‌0‌ ‌A‌M‌
         </div>
-        {/* https://support.google.com/calendar/answer/41207?hl=en */}
-        <iframe
-            src="https://calendar.google.com/calendar/embed?src=f6368e2kcfqibv4udhjn08ck4k%40group.calendar.google.com&ctz=America%2FLos_Angeles"
-            style= {{ border: 0 }}
-            width="800"
-            height="600"
-            frameborder="0"
-            scrolling="no">
-        </iframe>
-        {/* Start date button */}
-        <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MGQ1NzhoMWJ0cGswN2I2OTk1Nm83dmQyZ28gZjYzNjhlMmtjZnFpYnY0dWRoam4wOGNrNGtAZw&amp;tmsrc=f6368e2kcfqibv4udhjn08ck4k%40group.calendar.google.com">
-            <img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif">
-            </img>
-        </a>        {/* End date button */}
-        <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=NjUxaWN2MGk3cmpqb3Z1azRmNGhjN244MzAgZjYzNjhlMmtjZnFpYnY0dWRoam4wOGNrNGtAZw&amp;tmsrc=f6368e2kcfqibv4udhjn08ck4k%40group.calendar.google.com">
-            <img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif">
-            </img>
-        </a>
-
     </div>
 )
 
