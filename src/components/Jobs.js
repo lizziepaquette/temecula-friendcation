850e31346319ee7eb7a812b32488f0222b6cd754
Lizzie Paquette <lizzie@brex.com>
2021-06-29 13:04:54 -0700

    job board

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,13 +1,18 @@
 import React from "react"
 import scriptLoader from "react-async-script-loader";
+import RepoCard from "react-repo-card";
+
 
 const Jobs = () => (
     <div>
         <h1> Meet the team</h1>
         <div class="github-card" data-github="lizziepaquette" data-width="400" data-height="" data-theme="default"></div>
         <div class="github-card" data-github="justjack555" data-width="400" data-height="" data-theme="default"></div>
-        {/* <div class="github-card" data-github="jenncummings" data-width="400" data-height="150" data-theme="default"></div> */}
+        <div class="github-card" data-github="jenncummings" data-width="400" data-height="" data-theme="default"></div>
         <div class="github-card" data-github="lizziepaquette/temecula-friendcation" data-width="400" data-height="" data-theme="default"></div>
+    <div style={{ width: "405px" }}>
+      <RepoCard username="lizziepaquette" repository="temecula-friendcation" />
+    </div>
     </div>
 );
 
