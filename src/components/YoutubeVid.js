cd4d5e2af5203da572ef94c0dfd4f51ec7ecd80d
Lizzie Paquette <lizzie@brex.com>
2021-06-30 22:01:28 -0700

    youtube vid has right aspectt ratio

--- src/components/YoutubeVid.js
+++ src/components/YoutubeVid.js
@@ -2,8 +2,8 @@ import React from "react"
 
 const YoutubeVid = () => (
     <div>
-    <iframe width="100%"
-        height="315"
+    <iframe
+        style={{ width: '100%', height: '51.25vw', marginRight: '10px'}}
         src="https://www.youtube.com/embed/Kktx2ph4UYc"
         title="YouTube video player"
         frameborder="0"
